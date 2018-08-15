9da51f166b4b79cf7629c809489982ecb56c7f00
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-08-15 18:40:51 -0400

    Added functionality to submit the recipe-edit form

--- recipe-app/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ recipe-app/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -64,8 +65,24 @@ export class RecipeEditComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log('in onSubmit method');
-    console.log(this.recipeForm);
+    // const recipe = new Recipe(
+    //   this.recipeForm.value['name'],
+    //   this.recipeForm.value['description'],
+    //   this.recipeForm.value['imagePath'],
+    //   this.recipeForm.value['ingredients']
+    // );
+
+    // if (this.editMode) {
+    //   this.recipeService.updateRecipe(this.id, recipe);
+    // } else {
+    //   this.recipeService.addRecipe(recipe);
+    // }
+
+    if (this.editMode) {
+      this.recipeService.updateRecipe(this.id, this.recipeForm.value);
+    } else {
+      this.recipeService.addRecipe(this.recipeForm.value);
+    }
   }
 
   onAddIngredient() {
