1e6b08e431c3454b7f21d87952d7b4704f306485
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-08-22 13:12:26 -0400

    Added functionality to sign up users

--- recipe-app/src/app/auth/auth.service.ts
+++ recipe-app/src/app/auth/auth.service.ts
@@ -1,5 +1,11 @@
+import * as firebase from 'firebase';
+
 export class AuthService {
   signupUser(email: string, password: string) {
-
+    console.log('creating user!');
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .catch(
+        error => console.log(error)
+      );
   }
 }
