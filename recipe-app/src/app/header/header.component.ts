994365d04e28f3d0cc4d2dd4d47e6efb025bccce
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-08-22 09:06:57 -0400

    Setup DataStorageService, basic PUT request

--- recipe-app/src/app/header/header.component.ts
+++ recipe-app/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
+import { Response } from '@angular/http';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataStoreService: DataStorageService) { }
 
   ngOnInit() {
+  }
+
+  onSaveData() {
+    this.dataStoreService.storeRecipes().subscribe(
+      (response: Response) => {
+        console.log(response);
+      }
+    );
   }
 }
