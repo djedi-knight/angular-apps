a5a61bc47f6960422ed5fbddac294a55058bb0ef
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-08-24 09:03:47 -0400

    Restructuring services into the core module

--- recipe-app/src/app/core/core.module.ts
+++ recipe-app/src/app/core/core.module.ts
@@ -5,6 +5,10 @@ import { HeaderComponent } from "./header/header.component";
 import { HomeComponent } from "./home/home.component";
 import { SharedModule } from "../shared/shared.module";
 import { AppRoutingModule } from "../app-routing.module";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { RecipeService } from "../recipes/recipe.service";
+import { DataStorageService } from "../shared/data-storage.service";
+import { AuthService } from "../auth/auth.service";
 
 @NgModule({
   declarations: [
@@ -18,6 +22,12 @@ import { AppRoutingModule } from "../app-routing.module";
   ],
   exports: [
     HeaderComponent
-  ]
+  ],
+  providers: [
+    ShoppingListService,
+    RecipeService,
+    DataStorageService,
+    AuthService
+  ],
 })
 export class CoreModule { }
