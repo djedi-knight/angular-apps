41e9379a2264315e5937f0d6aae7c363597dafa4
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-09-09 19:35:04 -0400

    Added route for layout component

--- materials-app/src/app/app-routing.module.ts
+++ materials-app/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
 import { TableComponent } from './table/table.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'table', component: TableComponent }
+  { path: 'layout', component: LayoutComponent },
+  { path: 'table', component: TableComponent },
 ];
 
 @NgModule({
