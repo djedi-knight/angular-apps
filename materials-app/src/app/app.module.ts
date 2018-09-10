03dda9571d540da0b42d1709badd0003d44bcf22
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-09-09 21:13:38 -0400

    Playing around with layout module

--- materials-app/src/app/app.module.ts
+++ materials-app/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './navigation/navigation.component';
+import { FlexLayoutModule } from "@angular/flex-layout";
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -24,6 +25,7 @@ import { LayoutComponent } from './layout/layout.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
     LayoutModule,
     MatToolbarModule,
     MatButtonModule,
